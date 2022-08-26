39dde03e38706deb7af25408007cdd55e5e61041
pulidovpe <pulidovpe.dev@gmail.com>
2022-08-26 08:39:41 -0500

    feat: Send parameters with sys module

---
+++ 01/send_params.py
@@ -0,0 +1,13 @@
+import sys
+ 
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+ 
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+ 
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+    print(sys.argv[i], end = " ")
+     
